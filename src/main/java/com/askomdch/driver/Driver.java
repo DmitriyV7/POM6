8638be59b7cd5583f5c811098f12b98287e79057
DmitriyV7 <verkh.dmitriy@gmail.com>
2024-04-14 20:38:35 -0500

    driver page changed

--- src/main/java/com/askomdch/driver/Driver.java
+++ src/main/java/com/askomdch/driver/Driver.java
@@ -16,11 +16,19 @@ public class Driver {
 
     public static WebDriver getDriver(String browser){
         if (driver == null){
+            try{
             switch (browser){
                 case "chrome":
-//                    WebDriverManager.chromedriver().setup();
-//                    ChromeOptions options = getChromeOptions();
-                    driver = new ChromeDriver();
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--headless"); // Run in headless mode
+                    options.addArguments("--no-sandbox"); // Bypass OS security model
+//                    options.addArguments("--disable-gpu"); // Applicable to windows os only
+                    options.addArguments("--start-maximized"); // Maximize the browser on start
+                    options.addArguments("--enable-automation");
+                    options.addArguments("--disable-infobars");
+                    options.addArguments("--disable-dev-shm-usage");
+                    driver = new ChromeDriver(options);
                     break;
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
@@ -36,21 +44,13 @@ public class Driver {
             driver.manage().deleteAllCookies();
             driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(50));
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+            }catch (Exception e){
+                System.err.println("Error initializing WebDriver: " + e.getMessage());
+            }
         }
 
-        return driver;
-    }
 
-    private static ChromeOptions getChromeOptions() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); // Run in headless mode
-        options.addArguments("--no-sandbox"); // Bypass OS security model
-//                    options.addArguments("--disable-gpu"); // Applicable to windows os only
-        options.addArguments("start-maximized"); // Maximize the browser on start
-        options.addArguments("enable-automation");
-        options.addArguments("--disable-infobars");
-        options.addArguments("--disable-dev-shm-usage");
-        return options;
+        return driver;
     }
 
     public static void quitDriver(){
